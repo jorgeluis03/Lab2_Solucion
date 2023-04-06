bb210debdd2243693afdd40f151556094849312a
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-05 22:49:32 -0500

    contro

--- LAB2/src/main/java/com/example/lab2/controller/ArtistaController.java
+++ LAB2/src/main/java/com/example/lab2/controller/ArtistaController.java
@@ -1,4 +1,27 @@
 package com.example.lab2.controller;
 
+import com.example.lab2.repository.ArtistaRespository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/artista")
 public class ArtistaController {
+    //##################################333
+    final ArtistaRespository artistaRespository;
+
+    public ArtistaController(ArtistaRespository artistaRespository) {
+        this.artistaRespository = artistaRespository;
+    }
+    //###################################################
+
+    @GetMapping("/lista")
+    public String listaartista(Model model){
+        
+
+
+    }
+
 }
