895b40c62eb117707d76fd0f9ad2f980e3b9b41f
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-06 10:24:10 -0500

    crud ok

--- LAB2/src/main/java/com/example/lab2/controller/IntegranteController.java
+++ LAB2/src/main/java/com/example/lab2/controller/IntegranteController.java
@@ -7,8 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/integrantes")
@@ -36,6 +38,21 @@ public class IntegranteController {
     public String guardar(Integrante integrante){
         integranteRepository.save(integrante);
         return "redirect:/integrantes/listar";
+
+    }
+    @GetMapping("/editar")
+    public String editar(Model model, @RequestParam("id") int id){
+        //Econtrar si existe el id del onjeto
+        Optional<Integrante> optIntegrante = integranteRepository.findById(id);
+        //si lo encontró entonces lo trae con get
+        if(optIntegrante.isPresent()){
+            Integrante integrante = optIntegrante.get();
+            //mandamos el objeto a la vista para editar
+            model.addAttribute("integrante",integrante );
+            return "integrante/formEditar";
+        }else {
+            return "redirect:/integrante/listar";
+        }
 
     }
 
