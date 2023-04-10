30f2fa1a0d8ac245c7fb7266ad36b96c6e4f42a4
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-09 23:18:37 -0500

    concierto hasta guardar

--- LAB2/src/main/java/com/example/lab2/controller/ConciertoController.java
+++ LAB2/src/main/java/com/example/lab2/controller/ConciertoController.java
@@ -1,4 +1,41 @@
 package com.example.lab2.controller;
 
+import com.example.lab2.entity.Concierto;
+import com.example.lab2.repository.ConciertoRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/conciertos")
 public class ConciertoController {
+    //##################################333
+    final ConciertoRepository conciertoRepository;
+
+    public ConciertoController(ConciertoRepository conciertoRepository) {
+        this.conciertoRepository = conciertoRepository;
+    }
+    //#####################################
+
+    @GetMapping("/listar")
+    public String listar(Model model){
+        List<Concierto> lsitaConcierto = conciertoRepository.findAll();
+        model.addAttribute("listaConciertos", lsitaConcierto);
+
+        return "concierto/listaConciertos";
+    }
+    @GetMapping("/nuevo")
+    public String nuevo (){
+        return "concierto/formNuevo";
+    }
+    @PostMapping("/guardar")
+    public String guardar (Concierto concierto){
+        conciertoRepository.save(concierto);
+        return "redirect:/conciertos/listar";
+    }
+
 }
